be6cdc4909ff8c371f9d9bd91f9b4f99181d0a64
bageljrkhanofemus <57874654+bageljrkhanofemus@users.noreply.github.com>
2020-03-29 02:30:35 -0500

    Reorganized directories

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/aryanagarwal/PycharmProjects/untitled2/venv"
+set -gx VIRTUAL_ENV "/Users/aryanagarwal/PycharmProjects/RowdyHacks(2)/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
